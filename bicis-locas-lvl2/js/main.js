e218dfd8611c6890933a76bfefca6050353673df
LilianaCanPor <liliana.canpor@gmail.com>
2016-10-26 12:29:22 -0500

    Comentarios agregados

--- bicis-locas-lvl2/js/main.js
+++ bicis-locas-lvl2/js/main.js
@@ -1,6 +1,10 @@
 	function validateForm(){
 		var spans = document.getElementsByClassName('error');
 		while(spans.length > 0)	{
+		/*el metodo de la variable span en su declaracion arrojara un arreglo 
+		con .length nos referimos al numero de elementosque hay dentro del arreglo
+		Mientras el numero de elementos del arreglo sea mayor a 0 entonces
+		del elemento padre spans[0] remueve a su hijo.*/	
 			spans[0].parentElement.removeChild(spans[0]);
 		}
 
@@ -12,17 +16,23 @@
 		select = document.getElementsByTagName('select')[0].value;
 
 		function validar(value,mensaje,div_index,validation){
+/*esta funcion mostrara los mensajes correspondientes de validacion para cada input y ademas 
+aplicara su REGEX gracias a la condicion que perimite mostrar o eliminar los mensajes cuando el valor del input
+sea el adecuado a la REGEX mediante el index que resulta de buscar a los divs que anidaran a los span(mensajes)*/
 				if (validation){
 					var span = document.createElement('span');
-					span.innerHTML = mensaje;
-					span.className = 'error';
-					document.getElementsByTagName('div')[div_index].appendChild(span);
+						span.innerHTML = mensaje;
+						span.className = 'error';
+						document.getElementsByTagName('div')[div_index].appendChild(span);
+					/*appendChild incrusta el elemento span en el div directamente en el html. 
+					pero solo mientras la funcion de ejecuta, osea que sera temporal*/
 					return false;
 				}
 			return true;
 		}
 		if (name  !==''){
 			validar(name,'La primera letra debe ser mayúscula',1,!(/^[A-Z][a-z]*$/).test (name));
+
 		}
 		if (lastname !=='') {
 			validar(lastname,'La primera letra debe ser mayúscula',2,!(/^[A-Z][a-z]*$/).test (lastname));
@@ -33,25 +43,15 @@
 		if (pass !=='') {
 			validar(pass,'Elija otra contraseña',4,pass.length < 6 || pass  == 'password' || /^\d{6}$/.test(pass));
 		}
+/*Estas condiciones permiten validar que el valor ingresado sea el correcto siempre y cuando exista un valor dentro del campo*/
+
 		validar(name,'Campo requerido',1,name=='');
 		validar(lastname,'Campo requerido',2,lastname=='');
 		validar(mail,'Campo requerido',3,mail=='');
 		validar(pass,'Campo requerido',4,pass=='');
 		validar(select,'Debes elegir una opcion',5,select=='0');
-
-		/*function validarError(value){
-			if(value !=='');
-			return validar(value, mensaje, div_index, validation);
-		}
-
-		validarError(validar(name,'La primera letra debe ser mayúscula',1,!(/^[A-Z][a-z]*$/).test (name)));
-		validarError(validar(lastname,'La primera letra debe ser mayúscula',2,!(/^[A-Z][a-z]*$/).test (lastname)));
-		validarError(validar(mail,'Ingrese un e-mail válido',3,!(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test (mail)));
-		validarError(validar(pass,'Elija otra contraseña',4,pass.length < 6 || pass  == 'password' || /^\d{6}$/.test(pass)));*/
-		
-
-		
-	}
+//Aqui la validacion es que el campo este completado, es la misma para todos
+}
 
 
 
